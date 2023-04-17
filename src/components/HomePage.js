901ba8bee63c3883ebb2983733955933eb3e22af
Mohammed Tarig <m7md.tarigg@gmail.com>
2023-04-18 01:53:01 +0300

    home page completed

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import HomeMain from "./HomeMain";
+import HomeMain from "./home/HomeMain";
 
 function HomePage() {
   return (
